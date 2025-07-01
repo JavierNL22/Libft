f686294d6d0ae02dfadd337c614ea74acad6c827
JavierNL22 <84963183+JavierNL22@users.noreply.github.com>
2025-07-01 11:02:11 +0200

    Add files via upload

--- fract-ol/fractol.c
+++ fract-ol/fractol.c
@@ -6,7 +6,7 @@
 /*   By: jnavalla <jnavalla@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 18:45:23 by jnavalla          #+#    #+#             */
-/*   Updated: 2025/06/29 01:04:01 by jnavalla         ###   ########.fr       */
+/*   Updated: 2025/06/30 18:21:31 by jnavalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void put_pixel(t_fractal *f, int x, int y, int color)
     *(int *)(f->addr + offset) = color;
 }
 
-void	draw_fractal(t_fractal *f)
+void	draw_mandelbrot(t_fractal *f)
 {
 	int		px, py;
 
@@ -35,6 +35,45 @@ void	draw_fractal(t_fractal *f)
 			double	c_re = (px - f->centerX) * f->scale;
 			double	c_im = (py - f->centerY) * f->scale;
 			double	z_re = 0, z_im = 0;
+			int		iter = 0;
+
+			while (z_re * z_re + z_im * z_im <= 4 && iter < f->max_iter)
+			{
+				double z_new = z_re * z_re - z_im * z_im + c_re;
+				z_im = 2 * z_re * z_im + c_im;
+				z_re = z_new;
+				iter++;
+			}
+
+			int color;
+			if (iter == f->max_iter)
+				color = 0x000000; // interior del conjunto
+			else
+			{
+				double t = (double)iter / f->max_iter;
+				int r = (int)(9 * (1 - t) * t * t * t * 255);
+				int g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
+				int b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
+				color = (r << 16) | (g << 8) | b;
+			}
+			put_pixel(f, px, py, color);
+		}
+	}
+	mlx_put_image_to_window(f->mlx, f->win, f->img, 0, 0);
+}
+
+void	draw_julia(t_fractal *f)
+{
+	int		px, py;
+	double	c_re = 0.0;
+	double	c_im = 0.8;
+	f->max_iter = calculate_max_iter(f->scale);
+	for (py = 0; py < HEIGHT; py++)
+	{
+		for (px = 0; px < WIDTH; px++)
+		{
+			double	z_re = (px - f->centerX) * f->scale;
+			double	z_im = (py - f->centerY) * f->scale;
 			int		iter = 0;
 
 			while (z_re * z_re + z_im * z_im <= 4 && iter < f->max_iter)
@@ -85,7 +124,7 @@ int mouse_hook(int button, int x, int y, t_fractal *f)
         f->centerX = x - c_re / f->scale;
         f->centerY = y - c_im / f->scale;
 
-        draw_fractal(f);
+        draw_julia(f);
     }
     return (0);
 }
@@ -113,7 +152,7 @@ int main()
     f.fast_mode = 0;
     mlx_mouse_hook(f.win, mouse_hook, &f);
     mlx_hook(f.win, 17, 0, close_window, &f);
-    draw_fractal(&f);
+    draw_julia(&f);
 
     mlx_loop(f.mlx);
 }
